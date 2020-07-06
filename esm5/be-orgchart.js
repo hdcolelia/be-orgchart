4d9eb2c8f1feca427ae6df6a47cb784258734022
Hector Daniel Colelia <hdcolelia@hotmail.com>
2020-07-05 23:36:30 -0300

    Added wip list...

--- esm5/be-orgchart.js
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmUtb3JnY2hhcnQuanMiLCJzb3VyY2VSb290Ijoibmc6Ly9iZS1vcmdjaGFydC8iLCJzb3VyY2VzIjpbImJlLW9yZ2NoYXJ0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOztHQUVHO0FBRUgsY0FBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEdlbmVyYXRlZCBidW5kbGUgaW5kZXguIERvIG5vdCBlZGl0LlxuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcHVibGljLWFwaSc7XG4iXX0=
