d8187c2e0f3bca1d6e649ff90178218e8faca3f8
Hector Daniel Colelia <hdcolelia@hotmail.com>
2020-07-25 13:54:32 -0300

    Layout OK

--- lib/components/index.d.ts
+++
@@ -1,1 +0,0 @@
-export * from './orgchart/be-orgchart.component';
--- public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/clasess';
-export * from './lib/components';
-export * from './lib/interfaces';
-export * from './lib/be-orgchart.module';
